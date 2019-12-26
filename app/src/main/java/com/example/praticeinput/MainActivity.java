15b8c8d0ecb3fff9a8e0a72570acd17b01ebb851
mina <mina077424@gmail.com>
2019-12-26 09:38:55 +0900

    add checkbox

--- app/src/main/java/com/example/praticeinput/MainActivity.java
+++ app/src/main/java/com/example/praticeinput/MainActivity.java
@@ -11,36 +11,44 @@ import java.text.NumberFormat;
 public class MainActivity extends AppCompatActivity {
 
     int numberOfCoffee=2;
+    int price=0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
 
-    /**
-     * This method is called when the order button is clicked.
-     */
     public void submitOrder(View view) {
-        String priceMessage="Total: "+ NumberFormat.getCurrencyInstance().format(numberOfCoffee*5)+"\nThank you!";
+        price=calculatePrice(numberOfCoffee,5);
+        createOrderSummary();
+    }
+
+    public void createOrderSummary(){
+        String priceMessage="Name: Mina\n"+"Quantity:"+numberOfCoffee+"\nTotal: "+ NumberFormat.getCurrencyInstance().format(price)+"\nThank you!";
         displayMessage(priceMessage);
+    }
+
+    private int calculatePrice(int quantity,int pricePerCup) {
+        int price = quantity * pricePerCup;
+        return price;
     }
 
     public void increment(View view){
         numberOfCoffee+=1;
-        display(numberOfCoffee);
+        displayQuantity(numberOfCoffee);
         displayPrice(numberOfCoffee*5);
     }
 
     public void decrement(View view){
         numberOfCoffee-=1;
-        display(numberOfCoffee);
+        displayQuantity(numberOfCoffee);
         displayPrice(numberOfCoffee*5);
     }
 
     /**
      * This method displays the given quantity value on the screen.
      */
-    private void display(int number) {
+    private void displayQuantity(int number) {
         TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
         quantityTextView.setText("" + number);
     }
@@ -49,14 +57,14 @@ public class MainActivity extends AppCompatActivity {
      * This method displays the given price on the screen.
      */
     private void displayPrice(int number) {
-        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        TextView priceTextView = (TextView) findViewById(R.id.order_summary_text_view);
         priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
     }
     /**
      * This method displays the given text on the screen.
      */
     private void displayMessage(String message) {
-        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        TextView priceTextView = (TextView) findViewById(R.id.order_summary_text_view);
         priceTextView.setText(message);
     }
 }
